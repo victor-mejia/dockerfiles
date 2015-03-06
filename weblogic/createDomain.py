6869f425899eaf0f9b009e319a2f5f1e42969025
Víctor Andrés Mejía Silva <victor.mejia@vasslatam.com>
2015-03-06 11:16:49 -0500

    Recursos instalación weblogic12c

---
+++ weblogic/createDomain.py
@@ -0,0 +1,1 @@
+createDomain('/home/oracle/products/Oracle_Home/wlserver/common/templates/wls/wls.jar','/home/oracle/products/Oracle_Home/user_projects/domains/base_domain','weblogic','weblogic123')
